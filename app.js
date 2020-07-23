818ba751f14a42f68686d9701c65156177f7aa62
InAudibleDayne <daynebechtold@hotmail.com>
2020-07-22 21:22:06 -0600

    added connection to new Mlab MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 app.use('/posts', () => {
     console.log('This is a middleware running')
-})
+});
 
 app.get('/', (req,res) => {
     res.send('server running')
+});
+
+//Connection test
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true , useUnifiedTopology: true }, () => {
+    console.log('connected to DB!');
 })
 
 app.listen(8080);
