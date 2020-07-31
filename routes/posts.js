03c0cfad213bb33912eb158cbdcaecbd5297b3d4
InAudibleDayne <daynebechtold@hotmail.com>
2020-07-31 08:34:23 -0600

    added asynchronous calling to server to improve usability, also added path to get all posts from post route

--- routes/posts.js
+++ routes/posts.js
@@ -2,28 +2,32 @@ const express = require('express');
 
 const router = express.Router();
 const Post = require('../models/Post');
+const { restart } = require('nodemon');
 
-router.get('/', (req, res) => {
-    res.send('We are on Posts');
+router.get('/', async (req, res) => {
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    }catch(err){
+        res.json({ message: err })
+    }
 });
 
 router.get('/:id', (req, res) => {
     res.send('Specific post');
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
-
-    post.save()
-    .then(data => {
-        res.json(data);
-    })
-    .catch(err => {
-        res.json({message: err})
-    })
+    try{
+    const savedPost = await post.save();
+    res.json(savedPost);
+    }catch(err){
+        res.json({ message: err });
+    }
 })
 
 module.exports = router;
