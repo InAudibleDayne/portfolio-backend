49bde9c7aa5aa757377597ca5990e4bd6f67ff3e
InAudibleDayne <daynebechtold@hotmail.com>
2020-07-31 09:01:28 -0600

    added universal cors policy

--- routes/posts.js
+++ routes/posts.js
@@ -44,4 +44,20 @@ router.delete('/:postId', async (req, res)=> {
     }
 })
 
+router.patch('/:postId', async(req, res) => {
+    try{
+    const updatePost = Post.updateOne(
+        { _id: req.params.postId}, 
+        { $set: 
+            {
+                title: req.body.title,
+                description: req.body.description
+            }
+        });
+    res.json(updatePost)
+    }catch(err){
+        res.json({message: err})
+    };
+})
+
 module.exports = router;
